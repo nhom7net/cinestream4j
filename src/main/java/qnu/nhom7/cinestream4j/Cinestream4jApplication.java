7e57bfa4a20070b4df7d20f5cc42a2f88774b184
Sora <34794115+sorae42@users.noreply.github.com>
2024-11-29 21:10:32 +0700

    chore: supabase setup deps

--- src/main/java/qnu/nhom7/cinestream4j/Cinestream4jApplication.java
+++ src/main/java/qnu/nhom7/cinestream4j/Cinestream4jApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import qnu.nhom7.cinestream4j.utils.Supabase;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +18,7 @@ public class Cinestream4jApplication {
 
     @GetMapping("/test")
     public String test(@RequestParam(value = "name", defaultValue = "cinestream4j") String appName) {
+
         return String.format("%s da setup thanh cong!", appName);
     }
 }
