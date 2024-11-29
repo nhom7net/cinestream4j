4662faf1f50d544e41dd2d1e798bc0e4e1769a6e
Sora <34794115+sorae42@users.noreply.github.com>
2024-11-29 19:50:13 +0700

    Initial setup & commit

---
+++ src/main/java/qnu/nhom7/cinestream4j/Cinestream4jApplication.java
@@ -0,0 +1,22 @@
+package qnu.nhom7.cinestream4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Cinestream4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cinestream4jApplication.class, args);
+        System.out.println("Truy cập tại http://localhost:8080");
+    }
+
+    @GetMapping("/test")
+    public String test(@RequestParam(value = "name", defaultValue = "cinestream4j") String appName) {
+        return String.format("%s da setup thanh cong!", appName);
+    }
+}
---
+++ src/test/java/qnu/nhom7/cinestream4j/Cinestream4jApplicationTests.java
@@ -0,0 +1,13 @@
+package qnu.nhom7.cinestream4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cinestream4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
