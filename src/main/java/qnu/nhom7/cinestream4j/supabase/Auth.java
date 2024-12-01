cd60ad944a74bbbdf6929c454723e2891daffccf
Sora <34794115+sorae42@users.noreply.github.com>
2024-12-02 01:54:58 +0700

    feat: login function

--- src/main/java/qnu/nhom7/cinestream4j/supabase/Auth.java
+++ src/main/java/qnu/nhom7/cinestream4j/supabase/Auth.java
@@ -1,5 +1,8 @@
 package qnu.nhom7.cinestream4j.supabase;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,9 +11,11 @@ import java.net.http.HttpResponse;
 
 public class Auth {
     private final HttpClient httpClient;
+    private final ObjectMapper mapper;
 
     public Auth() {
         this.httpClient = HttpClient.newHttpClient();
+        this.mapper = new ObjectMapper();
     }
 
     public String signUp(String email, String password) throws IOException, InterruptedException {
@@ -22,7 +27,14 @@ public class Auth {
                 .build();
 
         HttpResponse<String> response = this.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+
+        JsonNode nameNode = this.mapper.readTree(response.body());
+
+        if (nameNode.get("access_token") == null) {
+            return nameNode.get("error_code").asText();
+        }
+
+        return nameNode.get("user").get("id").asText();
     }
 
     public String signIn(String email, String password) throws IOException, InterruptedException {
@@ -32,8 +44,15 @@ public class Auth {
                 .header("apikey", ConnectionString.getServiceKey())
                 .POST(HttpRequest.BodyPublishers.ofString("{\"email\":\"" + email + "\", \"password\":\"" + password + "\"}"))
                 .build();
-
         HttpResponse<String> response = this.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+
+        System.out.println(response.body());
+        JsonNode nameNode = this.mapper.readTree(response.body());
+
+        if (nameNode.get("access_token") == null) {
+            return nameNode.get("error_code").asText();
+        }
+
+        return nameNode.get("user").get("id").asText();
     }
 }
