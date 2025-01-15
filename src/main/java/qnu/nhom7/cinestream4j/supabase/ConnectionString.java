271dee540441ed391cd6ec6ec12ca133ad9456eb
Sora <34794115+sorae42@users.noreply.github.com>
2025-01-15 23:36:01 +0700

    chore: new supabase database

--- src/main/java/qnu/nhom7/cinestream4j/supabase/ConnectionString.java
+++ src/main/java/qnu/nhom7/cinestream4j/supabase/ConnectionString.java
@@ -1,8 +1,8 @@
 package qnu.nhom7.cinestream4j.supabase;
 
 public class ConnectionString {
-    private final static String supabaseUrl = "https://qmtfowdtlfcfgnihkyjf.supabase.co";
-    private final static String serviceKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtdGZvd2R0bGZjZmduaWhreWpmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTc2Mjg5OSwiZXhwIjoyMDQ1MzM4ODk5fQ.grIutHxyjT2S0kvhYv80tmX6zCoCIWLNKghDKrLCyDI";
+    private final static String supabaseUrl = "https://vfplyrnitdcuwmkjctrn.supabase.co";
+    private final static String serviceKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmcGx5cm5pdGRjdXdta2pjdHJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NTg2MTYsImV4cCI6MjA1MjUzNDYxNn0.HzwfTVcMurHpo6YmydA6pB_AFTlb3GTe3qoucUzKGCU";
 
     public static String getServiceKey() {
         return serviceKey;
