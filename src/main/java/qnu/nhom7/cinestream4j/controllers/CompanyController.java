699a321471e7c37cf6ef51cc827ca97f788ffcae
Sora <34794115+sorae42@users.noreply.github.com>
2025-01-17 00:05:07 +0700

    Revert "Mover"

--- src/main/java/qnu/nhom7/cinestream4j/controllers/CompanyController.java
+++ src/main/java/qnu/nhom7/cinestream4j/controllers/CompanyController.java
@@ -1,30 +1,13 @@
 package qnu.nhom7.cinestream4j.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.IOException;
-import java.util.Map;
-
-import qnu.nhom7.cinestream4j.services.tmdb.Discover;
 
 @Controller
 public class CompanyController {
 
     @GetMapping("/company")
-    public String showCompanyPage(@RequestParam String companyId, Model model) throws IOException, InterruptedException {
-        // Lấy thông tin hãng phim
-        Map<String, Object> companyInfo = Discover.getCompanyDetails(companyId);
-
-        // Lấy danh sách phim của hãng
-        var companyMovies = Discover.getMoviesByCompany(companyId);
-
-        // Truyền dữ liệu sang view
-        model.addAttribute("company", companyInfo);
-        model.addAttribute("movies", companyMovies);
-;
-        return "company";
+    public String showCompanyPage() {
+        return "company";  // Điều hướng tới company.html
     }
 }
--- src/main/java/qnu/nhom7/cinestream4j/controllers/NowPlayingController.java
+++ src/main/java/qnu/nhom7/cinestream4j/controllers/NowPlayingController.java
@@ -1,24 +1,13 @@
 package qnu.nhom7.cinestream4j.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import qnu.nhom7.cinestream4j.services.tmdb.Discover;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 @Controller
 public class NowPlayingController {
 
     @GetMapping("/nowplaying")
-    public String showNowPlayingPage(Model model) throws IOException, InterruptedException {
-        // Lấy danh sách phim đang chiếu từ API
-        ArrayList nowPlayingMovies = Discover.getNowPlaying();
-
-        // Đưa danh sách phim vào model để hiển thị trên view
-        model.addAttribute("movies", nowPlayingMovies);
-
+    public String showNowPlayingPage() {
         return "nowplaying";  // Điều hướng tới nowplaying.html
     }
 }
