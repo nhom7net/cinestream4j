dafd1f7a9d407fde86bbc4bea93a082e4f58e14e
Sora <34794115+sorae42@users.noreply.github.com>
2025-01-17 00:05:07 +0700

    Revert "Mover"

--- src/main/java/qnu/nhom7/cinestream4j/controllers/TopRatedController.java
+++ src/main/java/qnu/nhom7/cinestream4j/controllers/TopRatedController.java
@@ -1,24 +1,13 @@
 package qnu.nhom7.cinestream4j.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import qnu.nhom7.cinestream4j.services.tmdb.Discover;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 @Controller
 public class TopRatedController {
 
     @GetMapping("/toprated")
-    public String showTopRatedPage(Model model) throws IOException, InterruptedException {
-        // Lấy danh sách phim được đánh giá cao từ API
-        ArrayList topRatedMovies = Discover.getTopRated();
-
-        // Đưa danh sách phim vào model để hiển thị trên view
-        model.addAttribute("movies", topRatedMovies);
-
-        return "toprated"; // Điều hướng tới toprated.html
+    public String showTopRatedPage() {
+        return "toprated";  // Điều hướng tới toprated.html
     }
 }
